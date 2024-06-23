b803b983e0ca4cc397f105511656411a448731b2
Teklu moges <teklumoges482@gmail.com>
2024-06-24 02:44:10 +0300

    Done

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+import Head from "next/head";
 import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
@@ -6,15 +6,22 @@ import Provider from "@components/Provider";
 export const metadata = {
   title: "Promptopia",
   description: "The best place to find prompts for your next project",
+  image: "/assets/images/logo.svg",
 };
 
 const Rootlayout = ({ children }) => {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <link rel="icon" href={metadata.image} />
+      </Head>
       <body>
         <Provider>
           <div className="main">
-            <div className="gradiant" />
+            <div className="gradient" />
           </div>
           <main className="app">
             <Nav />
