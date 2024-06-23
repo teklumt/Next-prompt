4e7c98c810e6dbc1f7e66edf0a3933d19046f82c
Teklu moges <teklumoges482@gmail.com>
2024-06-23 17:08:28 +0300

    some changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,3 @@
-export const dynamic = "force-dynamic";
-
 import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,3 @@
-export const dynamic = "force-dynamic";
-
 import Feed from "@components/Feed";
 
 const Home = () => {
