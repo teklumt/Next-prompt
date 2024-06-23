6227225714cb4ce36345acc70242a19f1b5d945a
Teklu moges <teklumoges482@gmail.com>
2024-06-23 17:17:07 +0300

    to make the the ssh work

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
+export const dynamic = "force-dynamic";
 
 export const metadata = {
   title: "Promptopia",
